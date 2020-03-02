79910d4764ca43b48ad45f56e871a82022635512
Stefani8511 <rougirel.stephanie@gmail.com>
2020-03-02 21:45:10 +0100

    style

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 // DOM - Document Object Model
+ /*
+let n;
+
+n = document;
+n = document.all
+n = document.all[1]
+n = document.head
+n = document.title
+n = document.URL
+*/
+//Style
+
+let liste;
+liste = document.querySelector('li');
+liste = document.querySelector('li:nth-child(3)');
+
+liste.style.color = "red";
+liste.style.backgroundColor = "blue";
+
+console.log(liste);
+
+
+
